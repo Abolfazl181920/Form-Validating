37c92209f6a9c105a3632d3a12b413412178974c
Abolfazl <abolfazlm181920@gmail.com>
2022-01-26 20:35:21 +0330

    form jsx and css

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <div>
-            Form
-        </div>
+            <form className="form">
+                <h2>Form Charity</h2>
+                <div>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="text" placeholder="Password" />
+                    <input type="number" placeholder="Charity" />
+                </div>
+                <button>Submit</button>
+            </form>
     );
 }
 
