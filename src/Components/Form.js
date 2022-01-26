b2a8323a48915f89432dd5f1c8b3a8aea65113fe
Abolfazl <abolfazlm181920@gmail.com>
2022-01-26 20:56:19 +0330

    get value of inputs

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ charity, setCharity ] = useState("");
+
+    const handleNameValue = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleEmailValue = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordValue = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const handleCharityValue = (event) => {
+        setCharity(event.target.value);
+    }
+
     return (
         <form className="form">
             <h2>Form Charity</h2>
-                <div>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="text" placeholder="Password" />
-                    <input type="number" placeholder="Charity" />
-                </div>
+            <div>
+                <input
+                    onChange={handleNameValue}
+                    type="text"
+                    placeholder="Name"
+                />
+                <input
+                    onChange={handleEmailValue}
+                    type="email"
+                    placeholder="Email"
+                />
+                <input
+                    onChange={handlePasswordValue}
+                    type="text"
+                    placeholder="Password"
+                />
+                <input
+                    onChange={handleCharityValue}
+                    type="number"
+                    placeholder="Charity"
+                />
+            </div>
             <button>Submit</button>
         </form>
     );
