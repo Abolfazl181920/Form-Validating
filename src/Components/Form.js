ad3f8fea311e49324f1aea7c1915d6d2038de206
Abolfazl <abolfazlm181920@gmail.com>
2022-01-26 20:36:13 +0330

    clean code

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Form = () => {
     return (
-            <form className="form">
-                <h2>Form Charity</h2>
+        <form className="form">
+            <h2>Form Charity</h2>
                 <div>
                     <input type="text" placeholder="Name" />
                     <input type="email" placeholder="Email" />
                     <input type="text" placeholder="Password" />
                     <input type="number" placeholder="Charity" />
                 </div>
-                <button>Submit</button>
-            </form>
+            <button>Submit</button>
+        </form>
     );
 }
 
