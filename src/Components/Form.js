7dff868ddabf53aae95b443eb9d84b7adb57dbcf
Abolfazl <abolfazlm181920@gmail.com>
2022-01-27 22:07:29 +0330

    add better code blocks

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,64 +1,52 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 const Form = () => {
 
-    const [ name, setName ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ error, setError ] = useState("FIELD IS EMPTY!!!");
-    const [ charity, setCharity ] = useState("");
+    const [ firstName, setFirstName ] = useState('');
+    const [ lastName, setLastName ] = useState('');
+    const [ charity, setCharity ] = useState('');
 
-    const handleNameValue = (event) => {
-        setName(event.target.value);
+    const handleFirstName = (event) => {
+        setFirstName(event.target.value);
     }
-
-    const handleEmailValue = (event) => {
-        setEmail(event.target.value);
+    const handleLastName = (event) => {
+        setLastName(event.target.value);
     }
-
-    const handlePasswordValue = (event) => {
-        setPassword(event.target.value);
-    }
-
-    const handleCharityValue = (event) => {
+    const handleCharity = (event) => {
         setCharity(event.target.value);
     }
 
-    const formValidating = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-
-        // I MUST USE REACT-TOASTIFY INSTEAD OF ALERT
-        // USE OR OPERATOR INSTEAD OF 4 IF
-        if (name === "") {
-            alert(error);
-        }
-
-        if (email === "") {
-            alert(error);
-        }
-
-        if (password === "") {
-            alert(error);
-        }
-
-        if (charity === "") {
-            alert(error);
+        if (firstName !== "" && lastName !== "" && charity >= 1000) {
+            alert('ok');
         }
     }
-
+    
     return (
-        <form onSubmit={formValidating} className="form">
+        <form onSubmit={handleSubmit} className="form">
             <h2>Form Charity</h2>
             <div>
-                <input onChange={handleNameValue} type="text" placeholder="Name" />
-                <input onChange={handleEmailValue} type="email" placeholder="Email" />
-                <input onChange={handlePasswordValue} type="text" placeholder="Password" />
-                <input onChange={handleCharityValue} type="number" placeholder="Charity £" />
+                <input
+                    onChange={handleFirstName}
+                    value={firstName}
+                    type="text"
+                    placeholder="FirstName"
+                />
+                <input
+                    onChange={handleLastName}
+                    value={lastName}
+                    type="text"
+                    placeholder="LastName"
+                />
+                <input
+                    onChange={handleCharity}
+                    value={charity}
+                    type="number"
+                    placeholder="Charity £"
+                />
             </div>
-            <Link to="/charitables">
-                <button>Submit</button>
-            </Link>
+            <button>Submit</button>
         </form>
     );
 }
